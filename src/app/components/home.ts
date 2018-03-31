7064e6136d2a1fdca256b750cf2c4d3e6f929ec2
Michael Prather <mike@skynet.shintech.ninja>
2018-03-31 07:15:13 +0000

    checkbox

--- src/app/components/home.ts
+++ src/app/components/home.ts
@@ -1,12 +1,13 @@
 export class Home {
-  public val
-  
-  constructor () {
+  constructor (public val, public isChecked) {
     this.val = false
+    this.isChecked = false
   }
   
   myFunction () {
     this.val = !this.val
+    
+    console.log(`isChecked? = ${this.isChecked}`)
     console.log(`click - val = ${this.val}`)
   }
 }
