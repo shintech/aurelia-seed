db0c900ac31a2f8c55180d55e3cfce6b77a33884
Michael Prather <mprather1@gmail.com>
2018-04-07 05:47:35 +0000

    new model form

--- src/app/components/form.ts
+++ src/app/components/form.ts
@@ -1,8 +1,12 @@
+import {inject} from 'aurelia-framework'
+import {HttpClient} from 'aurelia-fetch-client'
+
 interface IOption {
   id: number
   val: string
 }
 
+@inject(HttpClient)
 export class FormComponent {
   firstName: string
   lastName: string
@@ -10,22 +14,39 @@ export class FormComponent {
   message: string
   optional: number = null
 
+  constructor (private http: HttpClient) {
+    this.http = http
+  }
+  
   options: IOption[] = [
     { id: 1, val: 'option1' },
     { id: 2, val: 'option2' },
     { id: 3, val: 'option3' }
   ]
   
-  submitForm () {
-    var newModel = { 
-      firstName: this.firstName,
-      lastName: this.lastName,
+  async submitForm () {
+    let http = this.http
+  
+    let newModel = { 
+      first_name: this.firstName,
+      last_name: this.lastName,
       email: this.email,
       optional: this.optional,
       message: this.message
     }
     
-    console.log(newModel);
- }
-  
+    try {
+      http.fetch('/api/models', {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(JSON.stringify(newModel), 'utf-8')
+        },
+        method: 'POST',
+        body: JSON.stringify(newModel)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
